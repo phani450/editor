51bef4660f38e9b985b25402656df08b2323e55f
Sai Phaneendra <phani.parlapalli@gmail.com>
2016-10-17 22:06:03 +0530

    Added Menu and Tabs

---
+++ code_snippets.py
@@ -0,0 +1,8 @@
+# For scrollable Text Editor
+
+textedit = tkst.ScrolledText(root)
+textedit.pack(fill=tk.BOTH, expand=tk.YES)
+
+# Creating Notebook/Tabbed Window
+
+notebook = ttk.Notebook(root)
--- micro_edit.py
+++ micro_edit.py
@@ -1,12 +1,32 @@
 import tkinter as tk
 import tkinter.scrolledtext as tkst
+import tkinter.ttk as ttk
+
+# Functions
+def newfile():
+	return 
 
 # Starting a UI
 root = tk.Tk()
 root.title("Editor for Microprocessor")
 
-textedit = tkst.ScrolledText(root)
+# For menubar
+root.option_add('*tearOff', tk.FALSE)
+menubar = tk.Menu(root)
+menu_new = tk.Menu(menubar)
+menubar.add_cascade(menu=menu_new, label = 'New File', command=newfile)
+root.config(menu=menubar)
+
+notebook = ttk.Notebook(root)
+tab1 = tk.Frame(notebook)
+notebook.add(tab1,text="tab one")
+textedit = tkst.ScrolledText(tab1)
 textedit.pack(fill=tk.BOTH, expand=tk.YES)
+tab2 = tk.Frame(notebook)
+notebook.add(tab2,text="tab two")
+textedit1 = tkst.ScrolledText(tab2)
+textedit1.pack(fill=tk.BOTH, expand=tk.YES)
+notebook.pack()
 
 
 
