3681e0bf6d0d2e4aba6a9c7498a35b902360b47b
Sai Phaneendra <phani.parlapalli@gmail.com>
2016-10-17 22:20:40 +0530

    New Tab

--- micro_edit.py
+++ micro_edit.py
@@ -1,10 +1,10 @@
 import tkinter as tk
 import tkinter.scrolledtext as tkst
 import tkinter.ttk as ttk
+import tkinter.messagebox as msgbox
 
 # Functions
-def newfile():
-	return 
+
 
 # Starting a UI
 root = tk.Tk()
@@ -12,21 +12,22 @@ root.title("Editor for Microprocessor")
 
 # For menubar
 root.option_add('*tearOff', tk.FALSE)
-menubar = tk.Menu(root)
-menu_new = tk.Menu(menubar)
-menubar.add_cascade(menu=menu_new, label = 'New File', command=newfile)
-root.config(menu=menubar)
-
 notebook = ttk.Notebook(root)
-tab1 = tk.Frame(notebook)
-notebook.add(tab1,text="tab one")
-textedit = tkst.ScrolledText(tab1)
-textedit.pack(fill=tk.BOTH, expand=tk.YES)
-tab2 = tk.Frame(notebook)
-notebook.add(tab2,text="tab two")
-textedit1 = tkst.ScrolledText(tab2)
-textedit1.pack(fill=tk.BOTH, expand=tk.YES)
+new_file_count = 0
+def newfile():
+	tab1 = tk.Frame(notebook)
+	notebook.add(tab1,text="New File ") # +str(new_file_count)
+	textedit = tkst.ScrolledText(tab1)
+	textedit.pack(fill=tk.BOTH, expand=tk.YES)
 notebook.pack()
+
+menu = tk.Menu(root)
+root.config(menu=menu)
+filemenu = tk.Menu(menu)
+menu.add_cascade(label="File", menu=filemenu)
+filemenu.add_command(label="New", command=newfile)
+# filemenu.add_command(label="Open...", command=open_command)
+# filemenu.add_command(label="Save", command=save_command)
 
 
 
